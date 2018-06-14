d348689697c4d259888e75e12e1897d6e51b6381
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-06-14 18:47:22 +0200

    Fix script

--- experiment/structured_pca.py
+++ experiment/structured_pca.py
@@ -17,7 +17,9 @@ from typing import List, NamedTuple
 from functional import pipe
 import numpy as np
 import numpy.lib.format as fmt
+from sklearn.externals import joblib
 from spdata.types import Coordinates, Dataset
+from tqdm import tqdm
 
 from algorithm import exims_pca, ModelledDataset
 
@@ -69,10 +71,10 @@ def merge_datasets(datasets: List[Dataset], cache_path: str, shift: int=10) -> \
     # https://stackoverflow.com/a/36749821/3067956
     n_spectra = sum([dataset.spectra.shape[0] for dataset in datasets])
     n_channels = datasets[0].spectra.shape[1]
-    merged_spectra = fmt.open_memmap(cache_path, mode='w', dtype=np.float32,
+    merged_spectra = fmt.open_memmap(cache_path, mode='w+', dtype=np.float32,
                                      shape=(n_spectra, n_channels))
     xs, ys, max_x, start = [], [], 0., 0
-    for dataset in datasets:
+    for dataset in tqdm(datasets, desc='Merging'):
         x = dataset.coordinates.x - dataset.coordinates.x.min() + shift + max_x
         max_x = x.max()
         y = dataset.coordinates.y - dataset.coordinates.y.min()
@@ -80,7 +82,7 @@ def merge_datasets(datasets: List[Dataset], cache_path: str, shift: int=10) -> \
         ys.append(y)
         end = start + dataset.spectra.shape[0]
         merged_spectra[start:end] = dataset.spectra
-    merged_spectra.flush()
+        merged_spectra.flush()
     xs, ys = np.hstack(xs), np.hstack(ys)
     merged_coordinates = Coordinates(x=xs, y=ys, z=np.zeros_like(xs))
     readonly_spectra = np.load(cache_path, mmap_mode='r')
@@ -93,7 +95,7 @@ def save(fname, obj):
 
 
 def save_result(modelled: ModelledDataset, results_root: str):
-    os.makedirs(results_root)
+    os.makedirs(results_root, exist_ok=True)
     fname = partial(os.path.join, results_root)
     save(fname('model.pkl'), modelled.model)
     np.save(fname('sorted_structness.npy'), modelled.features.score)
@@ -101,22 +103,28 @@ def save_result(modelled: ModelledDataset, results_root: str):
     np.save(fname('selection.npy'), modelled.features.selection)
     np.savetxt(fname('threshold.txt'), np.array([modelled.features.threshold]))
     np.save(fname('structured_dataset.npy'), modelled.structured_dataset)
-    np.save(fname('transformed_dataset.npy'), modelled.transformed_dataset)
-    save(fname('full_result.pkl'), modelled)
+    joblib.dump(modelled.transformed_dataset, fname('transformed_dataset.jbl'))
+    joblib.dump(modelled, fname('full_result.jbl'))
 
 
 if __name__ == '__main__':
-    arguments = Arguments(*sys.argv[1:5])
+    arguments = Arguments(*sys.argv[1:6])
     os.makedirs(arguments.results_root)
     datasets = [
         load_dataset(path, arguments.data_suffix, arguments.metadata_suffix)
         for path in subdirectories(arguments.datasets_root)
     ]
     merged_dataset = merge_datasets(datasets, arguments.cache_path)
-    print(f'Merged {len(datasets)} datasets with '
+    print(f'Merged {len(datasets)} datasets with total of '
           f'{merged_dataset.spectra.shape[0]} spectra.')
-    number_of_components = int(.02 * merged_dataset.spectra.shape[1])
+    number_of_components = merged_dataset.spectra.shape[1]
     print(f'Estimating {number_of_components} components...')
-    modelled = exims_pca(merged_dataset, pca_components=number_of_components)
+    modelled = exims_pca(merged_dataset, pca_components=number_of_components,
+                         random_state=0)
+    print(f'Selected {modelled.features.selection.sum()} features with'
+          f' structness over {modelled.features.threshold}')
+    print('Saving result')
     save_result(modelled, arguments.results_root)
+    print(f'Removing cache from {arguments.cache_path}')
+    del merged_dataset
     os.remove(arguments.cache_path)
